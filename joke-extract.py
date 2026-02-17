3c8c63891fa879170719c5882767daac33faa3f9
John Price <jp_github@gcfl.net>
2026-02-16 22:54:15 -0600

    Change log format

--- joke-extract.py
+++ joke-extract.py
@@ -20,7 +20,8 @@ import subprocess
 # Configure logging to stderr for visibility in pipelines
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s'
+    format='%(asctime)s %(levelname)s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
 )
 
 # Import parsers dynamically
